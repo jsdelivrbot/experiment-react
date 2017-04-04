e7d2fb9224c45a9280f73fb0349f47bd65a0bf28
Guilherme Santos <guilherme.goncalvessantos@gmail.com>
2017-04-04 16:21:08 -0300

    list working

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,15 +4,26 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
+    this.handleChange = this.handleChange.bind(this);
+
     this.state = { term: '' };
+  }  
+
+  handleChange(event) {
+    this.setState({
+      term: event.target.value
+    });
   }
 
   render() {
-    return <input onChange={this.onInputChange} />;
-  }
-
-  onInputChange(event) {
-    console.log(event.target.value);
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={this.handleChange}
+        />
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyC6oAQfQ74JKv-D3NQvuBlXL9gjiMq5SqA';
 
 // Create a new component. Produce some HTML
-const App = function() {
-  
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      // this.setState({ videos: videos });
+    });
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 // Take the component and put it in the DOM
